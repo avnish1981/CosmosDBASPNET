1b424ee6074bd886a151bcd4672ccada484c9b64
avnish1981 <avnish.choubey@gmail.com>
2020-05-27 15:36:01 +0530

    Add project files.

---
+++ CosmosApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CosmosApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
