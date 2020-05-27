c73247110f810c1b46546103605db01eb4707d84
avnish1981 <avnish.choubey@gmail.com>
2020-05-27 16:19:48 +0530

    Initial Commit

--- CosmosApp/Program.cs
+++ CosmosApp/Program.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.Azure.Cosmos;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +12,25 @@ namespace CosmosApp
     {
         static void Main(string[] args)
         {
+            QueryForDocument().Wait();
+        }
+
+        private async  static Task QueryForDocument()
+        {
+            var endpoint = ConfigurationManager.AppSettings["CosmosEndPoint"];
+            var masterkey = ConfigurationManager.AppSettings["CosmosMasterkey"];
+            using (var client = new CosmosClient(endpoint, masterkey))
+            {
+                var container = client.GetContainer("mydb", "mystore");
+                var sql = "SELECT * FROM c";
+                var iterator = container.GetItemQueryIterator<dynamic>(sql);
+                var page = await iterator.ReadNextAsync();
+                foreach( var doc in page )
+                {
+                    Console.WriteLine($"empId { doc.emp.id} has empName {doc.emp.Name}");
+                    Console.ReadLine();
+                }
+            }
         }
     }
 }
